5e186855f11c1c275d8077fe560cb78010609d63
Lu-SG <lucassilvagarcia02@gmail.com>
2024-07-23 15:16:25 -0300

    Adicionar Programação A B C

--- Programação C/Programação_C/.ipynb_checkpoints/start1-checkpoint.sh
+++
@@ -1,1 +0,0 @@
-docker run -u root --rm -e GRANT_SUDO=yes -e NB_GID=100 -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook start-notebook.sh --NotebookApp.token='python'
