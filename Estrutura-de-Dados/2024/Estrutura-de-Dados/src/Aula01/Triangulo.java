5fcc641800856f0edb63d4aafd4f890f0422a52e
Lu-SG <lucassilvagarcia02@gmail.com>
2024-07-31 18:42:34 -0300

    Add

---
+++ Estrutura-de-Dados/2024/Estrutura-de-Dados/src/Aula01/Triangulo.java
@@ -0,0 +1,18 @@
+package Aula01;
+
+public class Triangulo {
+
+    double Base;
+    double Altura;
+    double Area;
+
+    public Triangulo(double Base, Double Altura){
+        this.Base = Base;
+        this.Altura = Altura;
+    }
+
+    public Double calculaArea(){
+        Area = (Base * Altura) / 2;
+        return Area;
+    }
+}
