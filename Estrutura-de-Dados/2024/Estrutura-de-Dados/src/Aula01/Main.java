a6e16ed491a09bb28a185842a920c4cd50c30bf9
Lu-SG <lucassilvagarcia02@gmail.com>
2024-07-31 18:44:58 -0300

    Teste

--- Estrutura-de-Dados/2024/Estrutura-de-Dados/src/Aula01/Main.java
+++ Estrutura-de-Dados/2024/Estrutura-de-Dados/src/Aula01/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!"+"\n");
+        System.out.printf("Hello and welcomeE!"+"\n");
 
 
         Triangulo tr1 = new Triangulo(10.0,10.0);
