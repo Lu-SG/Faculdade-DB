c30fee5560c628fe9f6fdf6c5c25f7691b6e57b4
Lu-SG <lucassilvagarcia02@gmail.com>
2024-08-13 21:50:29 -0300

    Adicionar Classe Pilha

---
+++ Estrutura-de-Dados/2024/Estrutura-de-Dados/src/Aula03/AppPilha.java
@@ -0,0 +1,26 @@
+package Aula03;
+import java.util.Scanner;
+public class AppPilha {
+    public static void main (String args[]){
+        /*
+        System.out.println("Insira Tamanho da Pilha");
+        Scanner scanner = new Scanner(System.in);
+        int tamanho = scanner.nextInt();
+
+        Pilha pilha = new Pilha(5);
+
+        for(int i = 0; i < tamanho; i++){
+            System.out.println("Insira Número:");
+            pilha.push(scanner.nextInt());
+        }
+
+        System.out.println(pilha.top());
+        for(int i = 0; i < tamanho; i++){
+            System.out.println(pilha.pop());
+        }
+        */
+
+
+
+    }
+}
