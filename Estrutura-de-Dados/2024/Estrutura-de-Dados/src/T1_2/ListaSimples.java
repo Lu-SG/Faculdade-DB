e32e344dcb0b2659e9db06b897cb04e981a7ee3f
Lu-SG <lucassilvagarcia02@gmail.com>
2024-09-15 17:20:58 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ Estrutura-de-Dados/2024/Estrutura-de-Dados/src/T1_2/ListaSimples.java
@@ -0,0 +1,152 @@
+package T1_2;
+public class ListaSimples {
+    public Nodo prim;
+
+    public ListaSimples() {
+        this.prim = null;
+    }
+
+    public boolean isEmpty() {
+        if (this.prim == null)
+            return true;
+        else
+            return false;
+    }
+
+    public void insereInicio(int elemento) {
+        Nodo novoNodo = new Nodo(elemento);
+        novoNodo.next = prim;
+        prim = novoNodo;
+    }
+
+    public Nodo removeInicio() {
+        Nodo nodoAux = prim;
+        if (!this.isEmpty()) {
+            prim = nodoAux.next;
+        }
+        return nodoAux;
+    }
+
+    public void insereFim(int elemento) {
+
+        Nodo nodoUltimo = this.prim;
+
+        if (this.prim != null) {
+            while (nodoUltimo.next != null) {
+                nodoUltimo = nodoUltimo.next;
+            }
+
+        }
+
+        Nodo novoNodo = new Nodo(elemento);
+
+        if (this.prim == null)
+            this.prim = novoNodo;
+        else
+            nodoUltimo.next = novoNodo;
+
+    }
+
+    public Nodo removeFim() {
+        Nodo nodoAux = this.prim;
+        Nodo nodoUltimo = this.prim;
+
+        if (!this.isEmpty()) {
+            while (nodoUltimo.next != null) {
+                nodoAux = nodoUltimo;
+                nodoUltimo = nodoUltimo.next;
+            }
+
+            if (nodoUltimo != nodoAux)
+                nodoAux.next = null;
+            else
+                this.prim = null;
+        }
+
+        return nodoUltimo;
+    }
+
+    public void mostraLista() {
+        System.out.println("mostra do primeiro ao ultimo");
+
+        Nodo nodoAtual = prim;
+
+        while (nodoAtual != null) {
+            nodoAtual.mostraNodo();
+            nodoAtual = nodoAtual.next;
+        }
+    }
+
+    public void insere(int elemento, int pos) {
+        if ((pos == 1) || (this.prim == null))
+            insereInicio(elemento);
+        else {
+            int cont = 1;
+            Nodo nodoAux = this.prim;
+            while (nodoAux.next != null) {
+                cont++;
+                nodoAux = nodoAux.next;
+            }
+
+            if (pos > cont)
+                insereFim(elemento);
+            else {
+                cont = 1;
+                nodoAux = this.prim;
+                while (cont < (pos - 1)) {
+                    cont++;
+                    nodoAux = nodoAux.next;
+                }
+
+                Nodo novoNodo = new Nodo(elemento);
+
+                novoNodo.next = nodoAux.next;
+
+                nodoAux.next = novoNodo;
+
+
+            }
+        }
+    }
+
+    public Nodo remove(int pos) {
+        Nodo nodoRetorno = null;
+
+        if (this.isEmpty()) {
+            return nodoRetorno;
+        }
+
+        if (pos == 1)
+            nodoRetorno = removeInicio();
+        else {
+            int cont = 1;
+            Nodo nodoAux = this.prim;
+            while (nodoAux.next != null) {
+                cont++;
+                nodoAux = nodoAux.next;
+            }
+
+            if (pos >= cont)
+                nodoRetorno = removeFim();
+            else {
+                cont = 1;
+                nodoAux = prim;
+                Nodo nodoAnt = prim;
+                while (cont < pos) {
+                    cont++;
+                    nodoAnt = nodoAux;
+                    nodoAux = nodoAux.next;
+                }
+
+                nodoAnt.next = nodoAux.next;
+
+                nodoRetorno = nodoAux;
+            }
+
+
+
+        }
+        return nodoRetorno;
+    }
+
+}
---
+++ Estrutura-de-Dados/2024/Estrutura-de-Dados/src/T1_2/Main.java
@@ -0,0 +1,105 @@
+package T1_2;
+import java.util.Scanner;
+
+/* FACULDADE DOM BOSCO DE PORTO ALEGRE
+ * SisInfo & ADS – Estrutura de Dados – Segundo T1
+ *
+ * Objetivo do Programa: Solucionar Problema T1_2
+ * Componentes do grupo: Lucas Garcia
+ * Data da entrega do arquivo fonte:
+ */
+public class Main {
+    public static void main(String[] args){
+        ListaSimples lista = new ListaSimples();
+        //colocar dinamico lendo tamanho da lista?
+        FilaCircular filaCircular = new FilaCircular(10);
+        Pilha pilha = new Pilha(10);
+        Pilha pilha2 = new Pilha(10);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao programa do T1_2!"+"\n");
+
+        int escolha = 0;
+        while (escolha != 5){
+                System.out.println("Favor, digitar operação desejada:"+"\n"+
+                        "1 - Inserir elemento na Lista Simples"+"\n"+
+                        "2 - Mostrar Pilha"+"\n"+
+                        "3 - Mostrar Fila"+"\n"+
+                        "4 - Calcular Pilha e Fila"+"\n"+
+                        "5 - Sair"+"\n"
+                );
+                escolha = scanner.nextInt();
+
+                switch (escolha){
+                    case(1):
+                        System.out.println("Favor, digitar operação desejada:"+"\n"+
+                                "1 - Inserir elemento no Início"+"\n"+
+                                "2 - Inserir elemento no Fim"+"\n"+
+                                "3 - Inserir elemento na 'x' posição desejada"+"\n"
+                        );
+
+                        int escolha_insercao = scanner.nextInt();
+                        int elemento = 0;
+
+                        switch (escolha_insercao){
+                            case(1):
+                                System.out.println("Favor, digitar elemento que deseja inserir:"+"\n");
+                                elemento = scanner.nextInt();
+                                lista.insereInicio(elemento);
+                                break;
+
+                            case(2):
+                                System.out.println("Favor, digitar elemento que deseja inserir:"+"\n");
+                                elemento = scanner.nextInt();
+                                lista.insereFim(elemento);
+
+                                break;
+
+                            case(3):
+                                System.out.println("Favor, digitar elemento que deseja inserir:"+"\n");
+                                elemento = scanner.nextInt();
+                                System.out.println("Favor, digitar posição desejada:"+"\n");
+                                int posicao = scanner.nextInt();
+                                lista.insere(elemento,posicao);
+                                break;
+                        }
+                        break;
+
+                    case(2):
+                        while (!pilha.isEmpty()){
+                            System.out.println(pilha.pop());
+                        }
+                    break;
+
+                    case(3):
+                        while (!filaCircular.isEmpty()){
+                            System.out.println(filaCircular.remove());
+                        }
+                        break;
+
+
+                    case(4):
+                        Nodo nodoAux = lista.prim;
+
+                        while (nodoAux != null) {
+                            if (nodoAux.dado % 2 != 0 && nodoAux.dado > 0) {
+                                pilha.push(nodoAux.dado); // Ímpar positivo vai para a pilha
+                            } else {
+                                pilha2.push(nodoAux.dado); // Par ou negativo vai para pilha2
+                            }
+                            nodoAux = nodoAux.next; // Avança para o próximo nó
+                        }
+
+                        while (!pilha2.isEmpty()) {
+                            filaCircular.insere(pilha2.pop()); // Transfere elementos da pilha2 para a fila circular
+                        }
+
+                        break;
+                }
+        }
+
+        System.out.println("Obrigado por utilizar esse programa, até a próxima ;)"+"\n");
+
+    }
+}
